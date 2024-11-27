8a8e3f41209f71c1918f3ed36b10cd4dea49d774
anamul hauqe <parvesmosarof2@gmail.com>
2024-11-27 20:57:43 +0600

    added cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 
 // middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3tilc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
