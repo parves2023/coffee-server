0033ab6df38ae63992bc19f5f1c45fad2bcd9de9
Parves Mosarof <135069947+parves2023@users.noreply.github.com>
2024-12-01 22:48:38 +0600

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3tilc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 //  const uri = `mongodb+srv://parvesmosarof32:DuXEcEzgKc8D4wMY@cluster0.3tilc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+// console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     //insert a coffee
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
@@ -242,10 +242,10 @@ app.put("/chat/:id", async (req, res) => {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
